a1999c6b95c26b6a647bb957238c66950f72cee6
zhar <bwfzbw@gmail.com>
2025-04-27 16:48:33 +0700

    testing

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+class produk{
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 100.000;
+
+}
+
+$produk1 = new produk();
+var_dump($produk1);
